769cd03194c10d92443f6255fa9fb11b3623e782
Amanda Elbaz <ae89091@rams.colostate.edu>
2022-09-06 19:12:02 -0600

    updated name and bio to about page for teaminfo

---
+++ client/src/components/About/teamInfo.js
@@ -0,0 +1,43 @@
+import memberPic from "../../static/images/Placeholder.jpg";
+import teamPic from "../../static/images/PlaceholderTeam.jpg";
+
+export const teamData =
+    {
+        teamName: "Team Name",
+        missionStatement: "Mission Statement",
+        imagePath: teamPic,
+    };
+
+
+export const memberData = [
+    {
+        name: "Amanda Elbaz",
+        bio: "I am presently in my 4th year of studying Computer Science here at CSU Online. I am in the general Computer Science concentration so I can dabble around in everything and have a more broad understanding of Computer Science and related fields and disciplines. After graduation, I hope to use my Arabic background and CS degree to work for the FBI or CIA and combine those two skills to work in new and developing AI security, ethical hacking, and many other things.",
+        homeTown: "Miami, Florida",
+        imagePath: memberPic
+    },
+    {
+        name: "Team Member 2 ",
+        bio: "Bio Here",
+        homeTown: "Hometown",
+        imagePath: memberPic
+    },
+    {
+        name: "Team Member 3",
+        bio: "Bio Here",
+        homeTown: "Hometown",
+        imagePath: memberPic
+    },
+    {
+        name: "Team Member 4",
+        bio: "Bio Here",
+        homeTown: "Hometown",
+        imagePath: memberPic
+    },
+    {
+        name: "Team Member 5",
+        bio: "Bio Here",
+        homeTown: "Hometown",
+        imagePath: memberPic
+    },
+];
